e4758bba855ab84bde2da7907c83a362755cc7f8
Ester Bruden <ester.bruden@gmail.com>
2021-10-13 16:43:52 -0700

    82 - List All Owners on OwnersIndexPage .Closes #16

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,13 +1,23 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 // Anoother way to use @RequestMaping, we put as a  class annotation with /owners as a parameter  and I can remove this part of the path in the method annotation @RequestMapping, this way /owner is going to be prefix to the other paths
 @RequestMapping({"/owners"})
 @Controller
 public class OwnerController {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/","/index","/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model)
+    {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }

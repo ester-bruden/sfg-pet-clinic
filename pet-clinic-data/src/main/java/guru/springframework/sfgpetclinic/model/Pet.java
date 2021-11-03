130da9f27d60550c52e00ce5bf08237e89a55033
Ester Bruden <ester.bruden@gmail.com>
2021-11-02 18:13:58 -0700

    124 - Enhance Owners with ContactInfo and Pets #34. Closes #33

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -3,8 +3,26 @@ package guru.springframework.sfgpetclinic.model;
 import java.time.LocalDate;
 
 public class Pet extends BaseEntity{
+    private String name;
+    private LocalDate birthDate;
     private PetType petType;
     private Owner owner;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
 
     public PetType getPetType() {
         return petType;
@@ -21,15 +39,4 @@ public class Pet extends BaseEntity{
     public void setOwner(Owner owner) {
         this.owner = owner;
     }
-
-    public LocalDate getBirthdate() {
-        return birthdate;
-    }
-
-    public void setBirthdate(LocalDate birthdate) {
-        this.birthdate = birthdate;
-    }
-
-    private LocalDate birthdate;
-
 }

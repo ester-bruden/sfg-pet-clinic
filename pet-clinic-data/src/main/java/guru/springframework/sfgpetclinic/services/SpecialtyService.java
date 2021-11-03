4281703d75be420c26091eaa140854267c64949e
Ester Bruden <ester.bruden@gmail.com>
2021-11-02 18:40:23 -0700

    125 - Create Spiciality Map Service Implementation. #32. Closes #32

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Specialty;
+
+public interface SpecialtyService extends CRUDService<Specialty, Long> {
+}
